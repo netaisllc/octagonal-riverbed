a23c73e99c71acf66462bc6f7b5121e41f48f1cb
Kevin McGee <kevin.mcgee@netais.com>
2015-10-16 10:36:09 -0700

    Implement site in full

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// Main.js
+'use strict;'
+
+requirejs.config({
+    paths: {
+       head: '../lib/reveal/lib/js/head.min',
+       reveal: '../lib/reveal/js/reveal'
+    }
+});
+
+require([ 'head', 'reveal' ],
+    function ( head, Reveal ) {
+
+    // App...
+    Reveal.initialize( {
+        center: true,
+        controls: true,
+        history: true,
+        progress: true,
+
+        transition: 'convex', // none/fade/slide/convex/concave/zoom
+
+        // Optional reveal.js plugins
+        dependencies: [
+            {
+                src: '../lib/reveal/lib/js/classList.js',
+                condition: function() {
+                    return !document.body.classList;
+                }
+            },
+            {
+                src: '../lib/reveal/plugin/markdown/marked.js',
+                condition: function() {
+                    return !!document.querySelector( '[data-markdown]' );
+                }
+            },
+            {
+                src: '../lib/reveal/plugin/markdown/markdown.js',
+                condition: function() {
+                    return !!document.querySelector( '[data-markdown]' );
+                }
+            },
+            {
+                src: '../lib/reveal/plugin/zoom-js/zoom.js',
+                async: true
+            }
+        ]
+    } );
+  });
